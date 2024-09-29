dce1c8caebb36e81d4c9e74529f4e69ddf44ae4a
Orivaldo Santana Jr <orivaldo@gmail.com>
2024-09-28 23:56:37 -0300

    Test with motors

---
+++ src/tests/move/main.py
@@ -0,0 +1,17 @@
+# Robot codes 
+from L9110URA import L9110URA
+import time 
+robot = L9110URA(13,12,5,23)
+timeMove = 2000
+print("Frente...")
+robot.passoFrente(timeMove)
+time.sleep_ms(1000)
+print("Re...")
+robot.passoRe(timeMove)
+time.sleep_ms(1000)
+print("Esquerda...")
+robot.passoEsquerda(timeMove)
+time.sleep_ms(1000)
+print("Direita...")
+robot.passoDireita(timeMove)
+time.sleep_ms(1000)
